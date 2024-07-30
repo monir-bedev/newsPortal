db4021280d1580816e732dafddc7c3a27806f967
“username” <emailaddr>
2024-07-30 16:06:44 +0600

    create category model

--- blog/models.py
+++ blog/models.py
@@ -19,28 +19,9 @@ class Category(DomainEntity):
     name = models.CharField(max_length=100)
     thumbnail = models.ImageField(upload_to="blog/thumbnails", blank=True, null=True)
 
+    class Meta:
+        verbose_name_plural = 'categories'
+        
     def __str__(self):
         return self.name
 
-# Create Post models
-
-class Post(DomainEntity):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="blog/%Y/%m/%d")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-
-    def __str__(self):
-        return self.title
-
-# Create comment models
-
-class Comment(DomainEntity):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    message = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
